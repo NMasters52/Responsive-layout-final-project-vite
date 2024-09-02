063a61cac4779fb49d5883b8c596fc6ee94ca59a
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-09-02 09:05:03 -0400

    skeleton markup, Footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Hero />
         <Features />
         <Main />
+        <Footer />
      </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+
+                <h2>Just scratching the surface</h2>
+
+                <div>
+
+                    <div>
+                        <h3></h3>
+                        <p></p>
+                    </div>
+
+                    <div>
+                        <h3></h3>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3></h3>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3></h3>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+        </footer>
+
+    )
+}
