dd92ce1a8a214da0615b317480e01098b522da55
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-09-02 08:29:38 -0400

    content added to mark up, Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,31 +6,66 @@ export default function Main() {
             <div>
 
                 <div className="main__content">
-                    <h3 className="main__header"></h3>
-                    <p className="main__text"></p>
+                    <h3 className="main__header">
+                        It doesn’t have to be so hard
+                    </h3>
+                    <p className="main__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                    dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
                     <img 
                         src="../public/images/img1.jpg" 
                         alt="picture of webpage diagrams drawn out" 
                         className="main__image"
                     />
-                    <p className="main__text"></p>
+                    <p className="main__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                    dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
                 </div>
 
                 <div className="main__cards">
 
                     <div className="card">
-                        <h3 className="card__header"></h3>
-                        <p className="card__text"></p>
+                        <h3 className="card__header">
+                        Break It Down
+                        </h3>
+                        <p className="card__text">
+                        Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit, sed do 
+                        eiusmod tempor incididunt ut 
+                        labore et dolore magna aliqua
+                        </p>
                     </div>
 
                     <div className="card">
-                        <h3 className="card__header"></h3>
-                        <p className="card__text"></p>
+                        <h3 className="card__header">
+                        Work Your Way Up
+                        </h3>
+                        <p className="card__text">
+                        Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit, sed do 
+                        eiusmod tempor incididunt ut 
+                        labore et dolore magna aliqua
+                        </p>
                     </div>
 
                     <div className="card">
-                        <h3 className="card__header"></h3>
-                        <p className="card__text"></p>
+                        <h3 className="card__header">
+                        Go After It
+                        </h3>
+                        <p className="card__text">
+                        Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit, sed do 
+                        eiusmod tempor incididunt ut 
+                        labore et dolore magna aliqua
+                        </p>
                     </div>
 
                 </div>
