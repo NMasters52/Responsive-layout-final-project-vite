8e00e829d61742a21f40f152f7a3f2f4ebfbac80
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-09-02 07:00:10 -0400

    content component

---
+++ src/components/Content.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default function Content() {
+    return(
+        <section>
+            <div className="container">
+                <div className="section__content">
+                    <h1 className="section__header">
+                    Mobile-first
+                    </h1>
+                    <p className="section__text">
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam.
+                    </p>
+                </div>
+                <div className="section__content">
+                    <h1 className="section__header">
+                    Mobile-first
+                    </h1>
+                    <p className="section__text">
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam.
+                    </p>
+                </div>
+                <div className="section__content">
+                    <h1 className="section__header">
+                    Mobile-first
+                    </h1>
+                    <p className="section__text">
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
